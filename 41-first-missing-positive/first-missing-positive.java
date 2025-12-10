bf9e8e06e16f1521276a2610d8374cd2454f3aad
NandhanaPR <nandhuprunni@gmail.com>
2025-12-10 08:58:19 +0530

    Time: 1 ms (99.95%) | Memory: 73.9 MB (16.66%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,9 +1,24 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
-        HashSet<Integer> set = new HashSet<>();
-        for (int n : nums) set.add(n);
-        int i = 1;
-        while (set.contains(i)) i++;
-        return i;
+        
+        int ans[] = new int[nums.length + 1];
+
+        // mark the presence of each positive integer in the input array
+        for(int i = 0; i < nums.length; i++){
+            int val = nums[i];
+            if(val > 0 && val <= nums.length) { // check to avoid index out of bounds
+                ans[val] = 1;
+            }
+        }
+
+        // find the smallest missing positive integer by checking the array of marked integers
+        for(int i = 1; i < ans.length; i++){
+            if(ans[i] == 0){
+                return i;
+            }
+        }
+
+        // if no missing positive integer is found, return the length of the array
+    return ans.length;
     }
 }
